f5d913c3d21a381b6386f1f31794c40cf3100278
shubham07h <shubham1609sharma@gmail.com>
2023-01-21 03:51:21 +0530

    commit

--- EmployeePayRoll/Program.cs
+++ EmployeePayRoll/Program.cs
@@ -6,13 +6,29 @@ namespace EmployeePayroll
     {
         static void Main(string[] args)
         {
+            while (true)
             {
 
+                Console.WriteLine("1.Create database\n2.CreateTable");
+                int option = Convert.ToInt32(Console.ReadLine());
                 EmployeeRepository employee = new EmployeeRepository();
-                employee.CreateDatabase();
-                employee.CreateTable();
-            }
+                switch (option)
+                {
+                    case 1:
 
+                        employee.CreateDatabase();
+                        break;
+                    case 2:
+
+                        employee.CreateTable();
+                        Console.WriteLine("Table created successfully");
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter valid choice");
+                        break;
+                }
+            }
         }
     }
 }
--- EmployeePayRoll/Repository/EmployeeRepository.cs
+++ EmployeePayRoll/Repository/EmployeeRepository.cs
@@ -12,7 +12,7 @@ namespace EmployeePayroll.Repository
     internal class EmployeeRepository
     {
 
-        SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial catalog=master;
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-(localdb)\MSSQLLocalDB;Initial catalog=EmployeePayroll;
                                   Integrated Security=true");
         public void CreateDatabase()
         {
@@ -24,12 +24,15 @@ namespace EmployeePayroll.Repository
         }
         public void CreateTable()
         {
-            SqlCommand cmd = new SqlCommand("Create Table EmployeeTable(id int primary key identity(1,1),Name varchar(200),Salary bigint,Startdate date,phone bigint," +
-                    "Emp_Address varchar(200),Department varchar(200),Gender varchar(10), Basic_Pay bigint," +
-                    "Deductions bigint,Taxable_Pay bigint,Income_Tax bigint,Net_Pay bigint )", connection);
+
+            SqlCommand cmd = new SqlCommand("Create table Employee(id int primary key identity(1,1),name varchar(200)," +
+                "PhoneNumber bigint,Address varchar(200),Gender char,Basic_Pay bigint,Deduction bigint,Taxable_Pay bigint," +
+                "Tax bigint,Net_Pay bigint,start_date date);", connection);
             connection.Open();
             cmd.ExecuteNonQuery();
-            Console.WriteLine("Table Created Successfully");
+
         }
+
+
     }
 }
